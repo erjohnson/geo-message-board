6fec826786fe9ce00685e6a3746444a692bbc635
Eric <erjohnson.pdx@gmail.com>
2014-10-03 15:29:30 -0700

    add messages view, update message params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,9 @@ class MessagesController < ApplicationController
   def index
 
   end
+
+private
+  def message_params
+    params.require(:message).permit(:content, :latitude, :longitude)
+  end
 end
