d8330c9a82418cf8cd0e53b4b9ce18e6a23d58e4
Eric <erjohnson.pdx@gmail.com>
2014-10-03 15:34:15 -0700

    update message views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,20 @@
 class MessagesController < ApplicationController
-  def index
 
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to messages_url, notice: "Message added!"
+    else
+      render 'new'
+    end
   end
 
 private
