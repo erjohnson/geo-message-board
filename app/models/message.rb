103b34ca3fc0356ce4984cda170da7d1c7f389a9
Eric <erjohnson.pdx@gmail.com>
2014-10-10 12:14:00 -0700

    geocode user & message by latitude & longitude

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ActiveRecord::Base
   validates_presence_of :content
   # validates_presence_of :latitude
   # validates_presence_of :longitude
+  reverse_geocoded_by :latitude, :longitude
 end
