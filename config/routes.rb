7c0ae83328101beb1485b73c2c7a581c0878e246
Eric <erjohnson.pdx@gmail.com>
2014-10-10 15:01:46 -0700

    change root urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
-  root 'messages#index'
 
   resources :messages
   resources :users
+
+  authenticated :user do
+    devise_scope :user do
+      root to: "messages#index", :as => "home"
+    end
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "static_pages#index", :as => "hello"
+    end
+  end
 end
